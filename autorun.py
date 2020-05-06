f303f26f0a71d4facf724c938a9efdda9c64db6f
jjdelcerro <jjdelcerro@administrador-Lenovo-G50-70>
2020-05-06 10:28:06 +0200

    Atrapados errores del autorun para que no intente hacer nada si no estan instalados los plugins de los que depende.

--- autorun.py
+++ autorun.py
@@ -3,13 +3,23 @@ import gvsig
 from gvsig import getResource
 from java.io import File
 from org.gvsig.tools import ToolsLocator
-from addons.RingMap.ringMapGeoprocess import RingMapGeoprocess
+
+try:
+  from addons.RingMap.ringMapGeoprocess import RingMapGeoprocess
+except:
+  import sys
+  ex = sys.exc_info()[1]
+  gvsig.logger("Can't load module 'RingMapGeoprocess'. " + str(ex), gvsig.LOGGER_WARN)#, ex)
+  RingMapGeoprocess = None
+
 
 def i18nRegister():
   i18nManager = ToolsLocator.getI18nManager()
   i18nManager.addResourceFamily("text",File(getResource(__file__,"i18n")))
   
 def main(*args):
+  if RingMapGeoprocess== None:
+    return
   i18nRegister()
   process = RingMapGeoprocess()
   process.selfregister("Scripting")
